1471dbb85403a350626e40914144cceea86b35e9
Chris L <stylesuxx@gmail.com>
2018-03-14 15:33:22 +0100

    Added Taxonomy Language relation #21

---
+++ server/server/models/TaxonomyLanguage.js
@@ -0,0 +1,43 @@
+import { Model, DataTypes } from 'sequelize';
+
+class TaxonomyLanguage extends Model {
+  static init(sequelize) {
+    return super.init({
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+
+      TaxonomyId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        notEmpty: true,
+        references: {
+          model: 'Taxonomies',
+          key: 'id',
+        },
+      },
+
+      LanguageId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        notEmpty: true,
+        references: {
+          model: 'Languages',
+          key: 'id',
+        },
+      },
+
+      vocable: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        notEmpty: true,
+        defaultValue: 0,
+      },
+    }, { sequelize });
+  }
+}
+
+export default TaxonomyLanguage;
