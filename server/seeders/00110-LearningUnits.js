5326df028dd53184f9b3cc161a85ee28aabe6ab3
Chris L <stylesuxx@gmail.com>
2018-03-15 10:08:59 +0100

    Seed a LearningUnit

--- server/seeders/00110-LearningUnits.js
+++ server/seeders/00110-LearningUnits.js
@@ -1,7 +1,17 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.bulkInsert('LearningUnits', [
-    // Objects to be inserted
-  ], {}),
+  up: (queryInterface, Sequelize) => queryInterface.rawSelect('Taxonomies', {
+    where: {
+      id: 1,
+    },
+  }, ['id'])
+    .then(user => queryInterface.bulkInsert('LearningUnits', [
+      {
+        id: 1,
+        rootId: 1,
+        UserId: user,
+        createdAt: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    ], {})),
 
   down: queryInterface => queryInterface.bulkDelete('LearningUnits', {
     // Where clause
