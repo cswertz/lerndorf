77262a38ef3b32f650d0551eae00ce1b837f760c
Chris L <stylesuxx@gmail.com>
2018-08-31 14:11:54 +0200

    Added helper function to check if user is self or has proper capability

--- server/server/helpers/auth.js
+++ server/server/helpers/auth.js
@@ -62,7 +62,31 @@ const hasCapability = (...allowed) => (req, res, next) => {
   });
 };
 
+const isSelfOrHasCapability = (...allowed) => (req, res, next) => {
+  if (req.user) {
+    if (parseInt(req.params.id, 10) === parseInt(req.user.id, 10)) {
+      return next();
+    }
+
+    const { capabilities } = req.user;
+    const permit = allowed.filter(item => capabilities.indexOf(item) > -1);
+
+    if (permit.length > 0) {
+      return next();
+    }
+
+    return res.status(403).send({
+      error: 'You do not have the capability to do this',
+    });
+  }
+
+  return res.status(401).send({
+    error: 'Not logged in.',
+  });
+};
+
 export {
+  isSelfOrHasCapability,
   hasCapability,
   isLoggedIn,
   hasRole,
