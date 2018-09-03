4fb9edfdf439293ad5610be217f47591d7e9eb82
Chris L <stylesuxx@gmail.com>
2018-09-03 18:15:22 +0200

    Configured react router, added route for registration page #60

---
+++ client/src/Register.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import logo from './logo.svg';
+
+const Register = () => (
+  <div className="Home">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <h1 className="App-title">Welcome to React</h1>
+    </header>
+    <p className="App-intro">
+      Placeholder for registration page intended to test the router
+    </p>
+  </div>
+);
+
+export default Register;
