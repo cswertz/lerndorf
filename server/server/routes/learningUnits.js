270ab4c6f567c9090bc132a972833e7d53a000ac
Chris L <stylesuxx@gmail.com>
2018-10-31 14:26:21 +0100

    Add Knowledge Units when querying Learning Units

--- server/server/routes/learningUnits.js
+++ server/server/routes/learningUnits.js
@@ -98,8 +98,88 @@ router.get('/:id', (req, res) => {
             model: models.User,
             attributes: ['id', 'username'],
           },
+          {
+            model: models.KnowledgeUnit,
+            attributes: [
+              'id',
+              'comment',
+              'objective',
+              'time',
+              'recommendedAge',
+              'visibleCourses',
+              'visibleLexicon',
+              'visiblePublic',
+              'lectorate',
+              'review',
+              'publish',
+              'suitableDumb',
+              'suitableDeaf',
+              'suitableBlind',
+            ],
+            include: [
+              {
+                as: 'msr',
+                model: models.Taxonomy,
+                attributes: ['id', 'type'],
+              },
+              {
+                as: 'kt',
+                model: models.Taxonomy,
+                attributes: ['id', 'type'],
+              },
+              {
+                as: 'cl',
+                model: models.Taxonomy,
+                attributes: ['id', 'type'],
+              },
+              {
+                as: 'ot',
+                model: models.Taxonomy,
+                attributes: ['id', 'type'],
+              },
+              {
+                as: 'mt',
+                model: models.Taxonomy,
+                attributes: ['id', 'type'],
+              },
+              {
+                as: 'el',
+                model: models.Taxonomy,
+                attributes: ['id', 'type'],
+              },
+              {
+                as: 'l',
+                model: models.Taxonomy,
+                attributes: ['id', 'type'],
+              },
+              {
+                as: 'author',
+                model: models.User,
+                attributes: ['id', 'username'],
+              },
+              {
+                as: 'Texts',
+                where: {
+                  nextId: null,
+                },
+                required: false,
+                model: models.Text,
+                attributes: ['id', 'content', 'nextId', 'prevId', 'rootId'],
+                include: [
+                  {
+                    model: models.Language,
+                    attributes: ['id', 'code', 'name'],
+                  },
+                ],
+              },
+            ],
+          },
         ],
       },
+    ],
+    order: [
+      [models.LearningUnit, models.KnowledgeUnit, 'id', 'asc'],
+      [models.LearningUnit, models.KnowledgeUnit, 'Texts', 'id', 'asc'],
     ],
   })
     .then(result => res.json(result));
