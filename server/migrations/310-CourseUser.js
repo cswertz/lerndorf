46e12f33fa3befe8d991a8a7364d82eb34489090
cswertz <christian@swertz.at>
2020-11-16 15:55:20 +0100

    Data Model for Courses Added

---
+++ server/migrations/310-CourseUser.js
@@ -0,0 +1,59 @@
+export default {
+  up: (queryInterface, DataTypes) => queryInterface.createTable('CourseUser', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false,
+    },
+
+    courseId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      notEmpty: true,
+      references: {
+        model: 'Courses',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+    },
+
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      notEmpty: true,
+      references: {
+        model: 'Users',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+    },
+
+    roleId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      notEmpty: true,
+      references: {
+        model: 'Roles',
+        key: 'id',
+      },
+      onDelete: 'cascade',
+    },
+
+    enrolment: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+
+    createdAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+
+    updatedAt: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  }),
+  down: queryInterface => queryInterface.dropTable('UserRole'),
+};
