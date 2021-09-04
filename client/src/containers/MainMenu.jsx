6bbcc0c28e2c96b5149e9112e5f63c92493b5089
Philipp Walter <philippwalter@pm.me>
2021-09-04 14:40:29 +0200

    remove "Create Course" button

--- client/src/containers/MainMenu.jsx
+++ client/src/containers/MainMenu.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Collapse from '@material-ui/core/Collapse';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
-// import { userFetchRoles } from '@actions';
-// import { hasCapability } from '@utils/user';
 import AdminMenu from '@components/UI/AdminMenu';
 // import Badge from '@components/UI/Badge';
 
@@ -24,57 +19,17 @@ const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
   },
-  nested: {
-    paddingLeft: theme.spacing(4),
-  },
-  nestedListHeader: {
-    // '&.is-open': {
-    //   background: theme.palette.primary.light,
-    // },
-  },
-  nestedList: {
-    // '&.is-open': {
-    //   background: theme.palette.primary.light,
-    // },
-  },
 }));
 
 const MainMenu = () => {
   const classes = useStyles();
-  const [open, setOpen] = useState({
-    courses: false,
-  });
-
-  const handleClick = (event, item) => {
-    event.stopPropagation();
-    setOpen((prevState) => ({ ...prevState, [item]: !prevState[item] }));
-  };
 
   return (
     <List className={classes.root} component="nav" aria-labelledby="nested-list">
-      <ListItem
-        className={`${classes.nestedListHeader} ${open.courses && 'is-open'}`}
-        button
-        divider={!open.courses}
-        onClick={(event) => handleClick(event, 'courses')}
-      >
+      <ListItem button divider component={Link} to="/learning-units">
         <ListItemText primary="My Courses" />
-        {open.courses ? <ArrowDropDownIcon /> : <ArrowRightIcon />}
+        <ArrowRightIcon />
       </ListItem>
-      <Collapse in={open.courses} timeout="auto" unmountOnExit>
-        <List
-          className={`${classes.nestedList} ${open.courses && 'is-open'}`}
-          component="div"
-          disablePadding
-        >
-          <ListItem button className={classes.nested} component={Link} to="/learning-units">
-            <ListItemText primary="Course Overview" />
-          </ListItem>
-          <ListItem button className={classes.nested} component={Link} to="/course/create">
-            <ListItemText primary="Create Course" />
-          </ListItem>
-        </List>
-      </Collapse>
 
       {/* <ListItem button divider component={Link} to="/learning-units">
         <ListItemText primary="Meine Aufgaben" />
