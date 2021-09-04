277e0c4287e1f98e45b9176d9d657c58aca256c5
Philipp Walter <philippwalter@pm.me>
2021-09-04 14:26:24 +0200

    remove dashboard content, add username

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
@@ -13,22 +14,12 @@ import Card from '@components/UI/Card';
 
 const useStyles = makeStyles((theme) => ({
   wrapper: {},
-  imageContainer: {
-    borderRadius: 9,
-    marginBottom: theme.spacing(3),
-    overflow: 'hidden',
+  title: {
+    marginBottom: theme.spacing(4),
 
-    [theme.breakpoints.up('lg')]: {
-      maxHeight: 230,
-      marginBottom: theme.spacing(6),
+    [theme.breakpoints.up('sm')]: {
+      marginBottom: theme.spacing(8),
     },
-  },
-  image: {
-    objectFit: 'cover',
-    width: '100%',
-  },
-  text: {
-    padding: theme.spacing(2, 0),
   },
   divider: {
     marginTop: theme.spacing(4),
@@ -61,22 +52,12 @@ const useStyles = makeStyles((theme) => ({
 
 const Dashboard = () => {
   const classes = useStyles();
+  const user = useSelector((state) => state.user);
 
   return (
     <div className={classes.wrapper}>
-      <div className={classes.imageContainer}>
-        {/* <picture></picture> */}
-        <img className={classes.image} src="https://via.placeholder.com/320x180" alt="Lerndorf" />
-      </div>
-
-      <Typography variant="h3">Welcome to Lerndorf!</Typography>
-
-      <Typography className={classes.text} variant="body1">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. A eros pretium, risus, in et,
-        congue. Eget dolor id fames et. Nunc nunc lectus etiam ante. Morbi aliquam congue tincidunt
-        ornare ut in risus dictum etiam. Nulla consectetur ornare faucibus semper faucibus cursus.
-        Adipiscing rhoncus quis sit non est dolor at a, ut. Tincidunt amet ullamcorper semper nulla
-        nisi eget imperdiet sit aliquet.
+      <Typography className={classes.title} variant="h3">
+        Welcome to Lerndorf, {user.user.username}!
       </Typography>
 
       <Button
