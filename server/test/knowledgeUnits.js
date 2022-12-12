abf4cc241dba112674ff0b0844610a9eea4af941
leganz <manuel.pirker-ihl@sushi.dev>
2022-12-12 08:00:03 +0100

    Fixed: old testing issues

--- server/test/knowledgeUnits.js
+++ server/test/knowledgeUnits.js
@@ -60,7 +60,7 @@ describe('KnowledgeUnit', () => {
         .end((err, res) => {
           res.should.have.status(200);
           res.body.should.be.a('array');
-          res.body.length.should.be.eql(2);
+          res.body.length.should.be.eql(1);
 
           done();
         });
